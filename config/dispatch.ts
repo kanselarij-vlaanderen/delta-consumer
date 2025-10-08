df85d6c4341236b6040c7255df59a903b2d130e0
Erika Pauwels <erika.pauwels@gmail.com>
2025-10-08 14:08:31 +0200

    Update fetch API

--- config/dispatch.ts
+++ config/dispatch.ts
@@ -1,6 +1,12 @@
+import fs from 'node:fs';
+import path from 'path';
+import { Readable } from 'node:stream';
+import { finished } from 'node:stream/promises';
+import fetcher from '../lib/fetcher';
 import { ChangeSet } from '../types';
-import { moveTriples, downloadShareLinks } from '../support';
+import { moveTriples } from '../support';
 import dispatchQuad from './dispatch-quad';
+import { SYNC_FILESHARE_ENDPOINT } from '../cfg';
 
 export default async function dispatch(changesets: ChangeSet[]) {
   for (const changeset of changesets) {
@@ -14,5 +20,41 @@ export default async function dispatch(changesets: ChangeSet[]) {
       inserts: triplesToInsert,
       deletes: triplesToDelete
     }]);
+  }
+}
+
+// Private
+
+async function downloadShareLinks(inserts: Quad[]) {
+  const shareLinks = new Set<string>();
+
+  const isShareUri = (uri: String) => uri.startsWith("share://")
+
+  inserts
+    .map((i) => i.subject.value)
+    .filter(isShareUri)
+    .forEach((i) => shareLinks.add(i));
+
+  inserts
+    .filter((i) => i.object.type === "uri")
+    .map((i) => i.object.value)
+    .filter(isShareUri)
+    .forEach((i) => shareLinks.add(i));
+
+  for (const shareLink of shareLinks)
+    await downloadFile(shareLink);
+}
+
+async function downloadFile(uri: string) {
+  const downloadUrl = `${SYNC_FILESHARE_ENDPOINT}?uri=${uri}`;
+  const filePath = uri.replace('share://', '/share/');
+
+  console.log(`Downloading file ${uri} from ${downloadUrl}`);
+  const response = await fetcher(downloadUrl);
+  if (response.ok) {
+    const writeStream = fs.createWriteStream(filePath);
+    await finished(Readable.fromWeb(response.body).pipe(writeStream));
+  } else {
+    console.error(`Failed to download file ${uri} (${response.status} ${response.statusText})`);
   }
 }
